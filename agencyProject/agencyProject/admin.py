1ce14586d7d6309cfb42a27c4298abb21fbb13df
Kris Bullins <krb278@nau.edu>
2015-02-19 22:02:02 -0700

    Registered Review form

--- agencyProject/agencyProject/admin.py
+++ agencyProject/agencyProject/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import form, share_form
+from .models import form, share_form, ReviewForm
 
 admin.site.register(form)
 admin.site.register(share_form)
+admin.site.register(ReviewForm)
